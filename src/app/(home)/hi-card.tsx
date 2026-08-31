d6885565984b5c36ecc04d564be41dc272da6a92
lulaalu <707839981@qq.com>
2026-08-31 14:49:37 +0800

    first commit

--- src/app/(home)/hi-card.tsx
+++ src/app/(home)/hi-card.tsx
@@ -51,7 +51,7 @@ export default function HiCard() {
 					<img src='/images/avatar.png' className='mx-auto rounded-full' style={{ width: 120, height: 120, boxShadow: ' 0 16px 32px -5px #E2D9CE' }} />
 				</Link>
 				<h1 className='font-averia mt-3 text-2xl'>
-					{greeting} <br /> I'm <span className='text-linear text-[32px]'>{username}</span> , Nice to <br /> meet you!
+					{greeting} <br /> I'm <span className='text-linear text-[32px]'>{username}</span> ,  <br />Wish you  all the best!
 				</h1>
 			</Card>
 		</HomeDraggableLayer>
